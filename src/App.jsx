cf1aefdfffd8e59cd358ce8180dddeaa5a70402f
Helen Kover <koverhelen@gmail.com>
2024-07-16 20:01:04 +0000

    first react project with D

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-black w-full h-screen">
+      <div className="flex items-center justify-center p-20">
+        <h1 className="text-white text-2xl">
+          This is Helen's Portfolio WebApp and it's under development. 🚧
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
